cea21eef29cbb5389cebd2a03803e675b5bdd92c
Adolfo Scheglel <88658339+Adolfo-schlegel@users.noreply.github.com>
2022-07-29 11:18:35 -0300

    nose

--- Build/server.js
+++ Build/server.js
@@ -5,30 +5,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const Lotes_1 = __importDefault(require("./routes/Lotes"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const Lotes_1 = __importDefault(require("./routes/Lotes"));
+/* import express from "express";
+import cors from "cors";
+import Lotes from "./routes/Lotes";
+import dotenv from "dotenv"; */
 dotenv_1.default.config();
-class main {
+class Server {
     constructor() {
         this.app = (0, express_1.default)();
         this.config();
         this.routes();
     }
     config() {
-        this.app.set("port", process.env.PORT || 7575);
+        this.app.set('port', process.env.PORT || 3000);
         this.app.use((0, cors_1.default)());
-        this.app.use(express_1.default.json());
-        this.app.use(express_1.default.urlencoded({ extended: false }));
     }
     routes() {
         this.app.use("/api", Lotes_1.default);
     }
     start() {
-        this.app.listen(process.env.PORT || this.app.get("port"), () => {
-            console.log("Server in port " + this.app.get("port"));
+        this.app.listen(process.env.PORT || this.app.get('port'), () => {
+            console.log('server on port ', this.app.get('port'));
         });
     }
 }
-const program = new main();
-program.start();
-//# sourceMappingURL=index.js.map
+const server = new Server();
+server.start();
+//# sourceMappingURL=server.js.map
---
+++ src/server.ts
@@ -0,0 +1,39 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import Lotes from "./routes/Lotes";
+
+/* import express from "express";
+import cors from "cors";
+import Lotes from "./routes/Lotes";
+import dotenv from "dotenv"; */
+
+dotenv.config()
+
+class Server {
+    private app: express.Application;
+
+    constructor() {
+        this.app = express()
+        this.config()
+        this.routes()
+    }
+
+    config(): void {
+        this.app.set('port', process.env.PORT || 3000);
+        this.app.use(cors());
+    }
+
+    routes(): void {
+        this.app.use("/api", Lotes)
+    }
+
+    start(): void {
+        this.app.listen(process.env.PORT || this.app.get('port'), () => {
+            console.log('server on port ', this.app.get('port'))
+        })
+    }
+}
+
+const server = new Server()
+server.start()
